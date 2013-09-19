671045eddbd8eab885179c5be567077150d5e900
Vlad Preda <vladpreda@WKT-UB-03.(none)>
2013-09-19 19:31:10 +0300

    problem 4

---
+++ 4.py
@@ -0,0 +1,20 @@
+import math
+
+def check_palindrome(nr):
+	return str(nr) == str(nr)[::-1]
+
+
+def find_palindromes(length):
+	min = int('1' + '0' * (length - 1))
+	max = int('9' * length) + 1
+	print min, max
+	maxPal = 0
+	for i in reversed(range(min, max)):
+		for j in reversed(range(min, max)):
+			nr = i * j
+			if check_palindrome(nr):
+				if (nr > maxPal):
+					maxPal = nr
+	return maxPal			
+
+print find_palindromes(3)
