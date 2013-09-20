8e3ed2b9daa01377d10bfd5f693c2a8a65281fc6
Preda Vlad <preda.vlad@yahoo.com>
2013-09-20 13:38:00 +0300

    some fixes

--- 9.py
+++ 9.py
@@ -2,12 +2,15 @@ import time
 
 start_time = time.time()
 
-# should run or about 30 seconds :)
-for i in range(1, 999):
-    for j in range(i, 999):
-        for k in range(j, 999):
-            if (i + j + k) == 1000:
-                if i * i + j * j == k * k:
-                    print i, j, k
+# should run or about 5 seconds
+def find_numbers():
+	for i in range(1, 999):
+	    for j in range(i, 999):
+	        for k in range(j, 999):
+	            if (i + j + k) == 1000:
+	                if i * i + j * j == k * k:
+	                    return i, j, k
+
+print find_numbers()
 
 print time.time() - start_time, "seconds"
