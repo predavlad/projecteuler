230072f685bdc0087c272630cb542cde984e83c0
Vlad Preda <vlad.preda@devops.innobyte.ro>
2013-10-11 16:22:14 +0300

    refactoring

--- projecteuler.net/38.py
+++ projecteuler.net/38.py
@@ -1,46 +1,39 @@
 import time
 
-# brute force FTW! 4 seconds
+# 0.04 seconds
 start_time = time.time()
 
-## optimized for this exercise
-def is_pandigital(nr, n):
+
+def is_pandigital(nr):
+    """
+    Check if a number is 1-9 pandigital
+    """
     nr = str(nr)
-    #beg = nr[0:n]
-    #end = nr[-n:]
-    for i in map(str, range(1, n + 1)):
-        if i not in nr:  # beg or i not in end:
+    for i in map(str, range(1, 10)):
+        if i not in nr:
             return False
     return True
 
 
 def is_pandigital_multiple(n, div_list):
-    new_nr = ''
-    for i in range(len(div_list)):
-        new_nr += str(int(div_list[i]) * n)
+    new_nr = ''.join([str(n*div_list[i]) for i in range(len(div_list))])
 
     if len(new_nr) != 9:
         return False
 
-    if is_pandigital(new_nr, 9):
+    if is_pandigital(new_nr):
         return new_nr
 
     return False
 
 
-assert is_pandigital_multiple(192, range(1, 4))
-assert is_pandigital_multiple(9, range(1, 6))
-
-n = 5
-max_pandigital_multiple = 0
-
-for i in range(1, 50000):
-    for j in range(2, 16):
+max_pand = 0
+for i in xrange(10 ** 3, 10 ** 4):
+    for j in xrange(2, 4):
         rez = is_pandigital_multiple(i, range(1, j + 1))
         if rez:
-            if max_pandigital_multiple < rez:
-                max_pandigital_multiple = rez
+            max_pand = max(max_pand, rez)
 
-print max_pandigital_multiple
+print max_pand
 
 print time.time() - start_time, "seconds"
